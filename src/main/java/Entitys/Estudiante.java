3e14048b0cee6c25579e319da9bb047642a4bfc0
abril troncoso <abrilpotitroncoso@gmail.com>
2025-10-05 20:58:49 -0300

    entidad estudiante implementada

--- src/main/java/Entitys/Estudiante.java
+++ src/main/java/Entitys/Estudiante.java
@@ -1,11 +1,104 @@
 package Entitys;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Estudiante {
     @Id
-    private int id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
     private String nombre;
+    @Column(nullable = false)
+    private String apellido;
+    @Column(nullable = false)
+    private String dni;
+    @Column(nullable = false)
+    private String genero;
+
+    @Column(name = "numero_documento", nullable = false, unique = true)
+    private String numeroDocumento;
+
+    @Column(name = "ciudad_residencia")
+    private String ciudadResidencia;
+
+    @Column(name = "libreta_universitaria", nullable = false, unique = true)
+    private String libretaUniversitaria;
+
+    public Estudiante() {}
+
+    public Estudiante(String nombre, String apellido, String dni, String genero, String numeroDocumento, String ciudadResidencia, String libretaUniversitaria, String ciudad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.genero = genero;
+        this.numeroDocumento = numeroDocumento;
+        this.ciudadResidencia = ciudadResidencia;
+        this.libretaUniversitaria = libretaUniversitaria;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getNumeroDocumento() {
+        return numeroDocumento;
+    }
+
+    public void setNumeroDocumento(String numeroDocumento) {
+        this.numeroDocumento = numeroDocumento;
+    }
+
+    public String getCiudadResidencia() {
+        return ciudadResidencia;
+    }
+
+    public void setCiudadResidencia(String ciudadResidencia) {
+        this.ciudadResidencia = ciudadResidencia;
+    }
+
+    public String getLibretaUniversitaria() {
+        return libretaUniversitaria;
+    }
+
+    public void setLibretaUniversitaria(String libretaUniversitaria) {
+        this.libretaUniversitaria = libretaUniversitaria;
+    }
 }
