a1c38c322d8f974a5e440e0f3c8b5ee7c3c75ef1
Luca Gorosito <lucagoro04@gmail.com>
2025-10-05 21:29:35 -0300

    Creacion entidad Carrera y EstudianteCarrera

---
+++ src/main/java/Entitys/Carrera.java
@@ -0,0 +1,48 @@
+package Entitys;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+
+@Entity
+public class Carrera {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idCarrera;
+
+    @Column
+    private String carrera;
+
+    @Column
+    private Integer duracion;
+
+    /// @OneToMany(mappedBy = "carrera", cascade = CascadeType.ALL)
+    /// private List<EstudianteCarrera> estudiantes = new ArrayList<>();
+
+    public Carrera() {}
+
+    public Carrera(String carrera, Integer duracion) {
+        this.carrera = carrera;
+        this.duracion = duracion;
+    }
+
+    public Integer getIdCarrera() {
+        return idCarrera;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+
+    public Integer getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+}
---
+++ src/main/java/Entitys/EstudianteCarrera.java
@@ -0,0 +1,91 @@
+package Entitys;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+public class EstudianteCarrera {
+    @EmbeddedId
+    private EstudianteCarreraId id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("estudianteId")
+    @JoinColumn
+    private Estudiante estudiante;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("carreraId")
+    @JoinColumn
+    private Carrera carrera;
+
+    @Column
+    private LocalDate fechaInscripcion;
+
+    @Column
+    private Integer antiguedad;
+
+    @Column
+    private Boolean graduado;
+
+    public EstudianteCarrera() {}
+
+    public EstudianteCarrera(Estudiante estudiante, Carrera carrera, LocalDate fechaInscripcion, int antiguedad) {
+        this.estudiante = estudiante;
+        this.carrera = carrera;
+        this.fechaInscripcion = fechaInscripcion;
+        this.antiguedad = antiguedad;
+        this.graduado = graduado;
+        this.id = new EstudianteCarreraId(estudiante.getId(), carrera.getId());
+    }
+
+    // Getters y Setters
+    public EstudianteCarreraId getId() {
+        return id;
+    }
+
+    public void setId(EstudianteCarreraId id) {
+        this.id = id;
+    }
+
+    public Estudiante getEstudiante() {
+        return estudiante;
+    }
+
+    public void setEstudiante(Estudiante estudiante) {
+        this.estudiante = estudiante;
+    }
+
+    public Carrera getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(Carrera carrera) {
+        this.carrera = carrera;
+    }
+
+    public LocalDate getFechaInscripcion() {
+        return fechaInscripcion;
+    }
+
+    public void setFechaInscripcion(LocalDate fechaInscripcion) {
+        this.fechaInscripcion = fechaInscripcion;
+    }
+
+    public Integer getAntiguedad() {
+        return antiguedad;
+    }
+
+    public void setAntiguedad(Integer antiguedad) {
+        this.antiguedad = antiguedad;
+    }
+
+    public Boolean getGraduado() {
+        return graduado;
+    }
+
+    public void setGraduado(Boolean graduado) {
+        this.graduado = graduado;
+    }
+
+}
